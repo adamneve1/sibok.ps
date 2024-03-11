b24ad616b39b18d737d93e620486244deacd4c7a
adamneve1 <ttrrash@gmail.com>
2024-03-11 13:43:44 +0700

    dashboard adam

--- routes/web.php
+++ routes/web.php
@@ -1,30 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-<<<<<<< HEAD
-Use App\Http\Controllers\HomeController;
-Use App\Http\Controllers\DashboardController;
-=======
-Use App\Http\Controllers\RegisterController;
->>>>>>> c1451fcc1d3f52fca017da9fb34ba2419af5b592
-
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
-<<<<<<< HEAD
-//Route::get('/', function () {
- //   return view('welcome');
-//});
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\DashboardController;
 
 Route::get('/', [HomeController::class, 'index']);
 Route::get('/contact', [HomeController::class, 'contact']);
-
 Route::get('/dashboard', [DashboardController::class, 'dashboard']);
-
-=======
-Route::get('/', [RegisterController::class, 'index']);
-Route::get('/contact', [RegisterController::class, 'contact']);
-
-?>
->>>>>>> c1451fcc1d3f52fca017da9fb34ba2419af5b592
